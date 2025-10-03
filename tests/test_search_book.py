7a197d5287a890f4cfda80443e04d04182f52661
YusufAq1 <yusufxaqrabawi@gmail.com>
2025-10-03 18:22:34 -0400

    Update test_search_book.py

--- tests/test_search_book.py
+++ tests/test_search_book.py
@@ -5,7 +5,7 @@ from library_service import search_books_in_catalog
 def test_search_book_valid_by_title():
     results = search_books_in_catalog("1984", "title")
     assert isinstance(results, list)
-    assert any("1984" in book["title"].lower() for book in results)
+    assert any(book["title"].lower() == "1984" for book in results)
 
 def test_search_book_valid_by_isbn():
     
