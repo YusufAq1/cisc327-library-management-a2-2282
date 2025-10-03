5696834277d6c04a11ef549a56d22370c9c029ab
YusufAq1 <yusufxaqrabawi@gmail.com>
2025-10-03 18:08:51 -0400

    Update test_return_book.py

--- tests/test_return_book.py
+++ tests/test_return_book.py
@@ -4,17 +4,15 @@ from library_service import borrow_book_by_patron
 
 def test_return_valid_book():
     
-    borrow_book_by_patron("123456", 4)
-    success, message = return_book_by_patron("123456", 4)
+    borrow_book_by_patron("123456", 1)
+    success, message = return_book_by_patron("123456", 1)
     assert success == True
     assert "has been returned" in message.lower()
 
 def test_return_valid_book2():
-    """Test returning a valid book after borrowing it first."""
     
-    
-    borrow_book_by_patron("654321", 4)
-    success, message = return_book_by_patron("654321", 4)
+    borrow_book_by_patron("654321", 2)
+    success, message = return_book_by_patron("654321", 2)
     assert success == True
     assert "has been returned" in message.lower()
 
@@ -25,8 +23,8 @@ def test_return_invalid_book():
     assert "book id not found" in message.lower()
 
 def test_return_invalid_patron():
-    """Test returning with an invalid patron (wrong length ID)."""
-    success, message = return_book_by_patron("54321", 3) 
+    """Test returning with an invalid patron ."""
+    success, message = return_book_by_patron("543217", 3) 
     assert success == False
    
     assert "not borrowed any books" in message.lower()
